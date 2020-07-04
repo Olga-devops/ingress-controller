9c51e61b12fb3a89add12fc895ebbe3e5a2ef62d
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-07-04 16:43:41 -0500

    Update module.tf

--- ingress-helm/module.tf
+++ ingress-helm/module.tf
@@ -2,7 +2,7 @@ module "ingress_controller" {
   source  = "fuchicorp/chart/helm" 
   deployment_name        = "ingress-controller"
   deployment_environment = "default"
-  deployment_endpoint    = "ingress-controller..${var.domain_name}"
+  deployment_endpoint    = "ingress-controller.${var.domain_name}"
   deployment_path        = "ingress-controller"
 
   # template_custom_vars   = {
